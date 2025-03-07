b4be62452e932469d1424f2ee85d0bb408fb29d2
songchengxuan <44460797+songchengxuan@users.noreply.github.com>
2025-03-07 13:49:16 +0800

    Update gplink.py

--- DjangoBi/gplink.py
+++ DjangoBi/gplink.py
@@ -3,10 +3,10 @@ import psycopg2
 
 def gp_connect():
     try:
-        db = psycopg2.connect(dbname="hodo_dw",
-                              user="etl",
-                              password="Asd123123!",
-                              host="10.10.67.160",
+        db = psycopg2.connect(dbname="xxx",
+                              user="xxx",
+                              password="xxxxxx",
+                              host="xxx.xxx.xxx.xxx",
                               port="5432")
         # connect()也可以使用一个大的字符串参数,
         # 比如”host=localhost port=5432 user=postgres password=postgres dbname=test”
